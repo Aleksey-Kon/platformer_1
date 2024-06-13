6d7627a408783bb180066c400376c41cb8ebe0d0
Aleksey-Kon <windowsviser@gmail.com>
2024-06-13 14:50:11 +0300

    13.06

---
+++ Assets/script/Player/PlayerConroller.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerConroller : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private float _speed,_jump,_input;
+    [SerializeField] private Animator _animator;
+    private bool _right = true;
+    public void SetMovePlayer(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<float>();
+    }
+    public void Jimp()
+    {
+        _rb.AddForce(new Vector2(0, _jump), ForceMode2D.Impulse);
+    }
+    private void PlayerScale(int i)
+    {
+        _right = !_right;
+        gameObject.transform.localScale = new Vector3(3 * i, 3, 1);
+    }
+    public void PlayerRun(int direction)
+    {
+        if(_right && direction == -1) {
+           PlayerScale(direction);
+        }
+        if (!_right && direction == 1)
+        {
+            PlayerScale(direction);
+        }
+        _rb.velocity = new Vector2(_speed * direction, 0);
+        //_rb.AddForce(new Vector2(_speed * direction, 0), ForceMode2D.Force);
+        _animator.SetFloat("Run", 1f);
+    }
+    private void FixedUpdate()
+    {
+        PlayerRun((int)_input);
+        if(_input == 0)
+        {
+            _animator.SetFloat("Run", 0f);
+        }
+    }
+}
